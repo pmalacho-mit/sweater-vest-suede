168cf97a981841d41f9fb781353c03c33b163d9e
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-12-06 19:45:10 +0000

    git subrepo pull (merge) release
    subrepo:
      subdir:   "release"
      merged:   "23f7df4"
    upstream:
      origin:   "https://github.com/pmalacho-mit/sweater-vest-suede.git"
      branch:   "release"
      commit:   "3d191ed"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "8bbc88c"

---
+++ release/template/Component.test.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Sweater } from "$sweater-vest-suede";
+  import Component from ".svelte";
+</script>
+
+<Sweater
+  body={async ({ set }) => {
+    set({});
+  }}
+>
+  {#snippet vest(pocket: {})}
+    <Component />
+  {/snippet}
+</Sweater>
