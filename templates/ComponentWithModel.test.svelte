97a09f2b3f7578a08e52d48feb5352494e03ad0e
GitHub Action <action@github.com>
2025-12-06 22:11:34 +0000

    Revert "updating template"
    This reverts commit 9ca937a4ba811fa849b2e75b54151e2d9680d180.

--- templates/ComponentWithModel.test.svelte
+++ templates/ComponentWithModel.test.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
   import { Sweater } from "_sweater-vest-suede";
-  import Component, { Model } from "_.svelte";
+  import Component from "_.svelte";
 </script>
 
 <Sweater
   body={async ({ set }) => {
-    const model = new Model();
-    set({ model });
+    set({});
   }}
 >
-  {#snippet vest({ model }: { model: Model })}
-    <Component {model} />
+  {#snippet vest(pocket: {})}
+    <Component />
   {/snippet}
 </Sweater>
