10c28ad98da6117cd36fd3f7cf796cffe891adf7
GitHub Action <action@github.com>
2025-12-06 22:26:34 +0000

    chore(suede): update release to trigger commits:\n 9182a376f9c3d358895a28effad90dd9b1940fdf
    Original commit messages:
    
      ---
      Commit: 9182a376f9c3d358895a28effad90dd9b1940fdf
      updating content
    
      ---
      Commit: 896247f5e26d6e26cde7bbe58fb80806c8ea7e9b
      adding file

---
+++ templates/ComponentWithContainer.test.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Sweater } from "_sweater-vest-suede";
+  import Component from "_.svelte";
+
+  class Pocket {
+    container = $state<HTMLDivElement>();
+  }
+</script>
+
+<Sweater
+  body={async ({ set }) => {
+    const pocket = set(new Pocket());
+  }}
+>
+  {#snippet vest(pocket: Pocket)}
+    <div bind:this={pocket.container}>
+      <Component />
+    </div>
+  {/snippet}
+</Sweater>
