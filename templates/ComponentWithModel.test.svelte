2b99b98465747c331a0f55e2bb258e230745c7c2
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-12-06 22:10:21 +0000

    Adding new ComponentWithModel file

---
+++ templates/ComponentWithModel.test.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Sweater } from "_sweater-vest-suede";
+  import Component from "_.svelte";
+</script>
+
+<Sweater
+  body={async ({ set }) => {
+    set({});
+  }}
+>
+  {#snippet vest(pocket: {})}
+    <Component />
+  {/snippet}
+</Sweater>
