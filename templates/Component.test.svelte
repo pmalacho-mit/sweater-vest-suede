56471f666d62939aac148d8ea3b1445fe825639c
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-12-06 23:13:52 +0000

    updating template

--- templates/Component.test.svelte
+++ templates/Component.test.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   import { Sweater } from "_sweater-vest-suede";
   import Component from "_.svelte";
+
+  class Pocket {}
 </script>
 
 <Sweater
   body={async ({ set }) => {
-    set({});
+    set(new Pocket());
   }}
 >
-  {#snippet vest(pocket: {})}
+  {#snippet vest(pocket: Pocket)}
     <Component />
   {/snippet}
 </Sweater>
