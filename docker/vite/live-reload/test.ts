00e5a6cd3bc094a8ece1965dd3c333a92aa79079
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2026-04-30 14:56:09 +0000

    preparing for live reload rewrite

--- docker/vite/live-reload/test.ts
+++ docker/vite/live-reload/test.ts
@@ -8,34 +8,36 @@ describe("live-reload", { concurrent: false }, async () => {
       .map((b) => b.textContent?.trim())
       .join(",");
 
-  const getLoadedAt = () => document.querySelector("#loaded-at")?.textContent;
+  const { open } = sessionSuite(import.meta.dirname);
 
-  const { open, config } = sessionSuite(import.meta.dirname);
+  const filesUnderTest = [
+    "Component.svelte",
+    "Component.test.svelte",
+    "release/Sweater.svelte",
+  ].map((files) => `/app/src/${files}`);
 
-  test("Editing Component.svelte triggers page reload", async () => {
-    const { evaluate, expectNoConsoleErrors, tabIndex } = await open();
+  for (const file of filesUnderTest)
+    test(`Editing ${file} triggers page reload?`, async () => {
+      const { evaluate, expectNoConsoleErrors } = await open();
 
-    const loadedAt = await evaluate(getLoadedAt);
-    expect(loadedAt).toBeDefined();
+      await poll(
+        async () => {
+          try {
+            const result = await evaluate(selectAllButtons);
+            if (!result) return false;
+            return (
+              result.includes("clicks: 3") &&
+              result.includes("clicks: 4") &&
+              result.includes("clicks: 5") &&
+              result.includes("clicks: 6")
+            );
+          } catch {
+            return false;
+          }
+        },
+        { timeout: 60_000 },
+      );
 
-    await poll(
-      async () => {
-        try {
-          const result = await evaluate(selectAllButtons);
-          if (!result) return false;
-          return (
-            result.includes("clicks: 3") &&
-            result.includes("clicks: 4") &&
-            result.includes("clicks: 5") &&
-            result.includes("clicks: 6")
-          );
-        } catch {
-          return false;
-        }
-      },
-      { timeout: 60_000 },
-    );
-
-    await expectNoConsoleErrors();
-  }, 90_000);
+      await expectNoConsoleErrors();
+    }, 90_000);
 });
