48a5321ce5a5f523dff34f4cde2d5f4aa07267a2
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2026-05-03 12:22:57 +0000

    small changes to browser control

--- release/suede/browser-control-container-suede/index.ts
+++ release/suede/browser-control-container-suede/index.ts
@@ -29,16 +29,6 @@ type Options = Partial<
   }
 >;
 
-export const tryRemove = async (
-  browser: Browser,
-  details?: Pick<Options, "container" | "log">,
-) => {
-  try {
-    await container.remove((details?.container ?? defaults.container)(browser));
-    if (details?.log) console.log(`Removed existing container for ${browser}`);
-  } catch {}
-};
-
 /**
  *
  * @param BROWSER
@@ -50,7 +40,8 @@ export const buildAndRun = async (BROWSER: Browser, details?: Options) => {
   const name = (details?.container ?? defaults.container)(BROWSER);
   const tag = (details?.image ?? defaults.image)(BROWSER);
 
-  await tryRemove(BROWSER, details);
+  await container.tryRemove(name);
+  if (details?.log) console.log(`Tried to remove container for ${BROWSER}`);
 
   if (details?.log) console.log(`Building image ${tag} from ${context}...`);
 
@@ -195,6 +186,13 @@ export const playwright = {
       ),
 };
 
+/**
+ *
+ * @param container
+ * @param session
+ * @param browser
+ * @returns
+ */
 export const sessionWithTabs = async (
   container: string,
   session: string,
@@ -213,10 +211,7 @@ export const sessionWithTabs = async (
    */
   const advance = () => {};
 
-  const withTabSelected = <Return>(
-    index: number,
-    fn: () => Return,
-  ): Promise<Awaited<Return>> => {
+  const withTabSelected = <Return>(index: number, fn: () => Return) => {
     const result = queue.then(async () => {
       await selectTab(index);
       return fn();
