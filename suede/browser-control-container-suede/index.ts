480aa3d23a3fff39cba753824dd1613b40acb53e
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2026-05-03 12:33:37 +0000

    Things got in a funny state

--- suede/browser-control-container-suede/index.ts
+++ suede/browser-control-container-suede/index.ts
@@ -29,16 +29,6 @@ type Options = Partial<
   }
 >;
 
-export const tryRemove = async (
-  browser: Browser,
-  details?: Pick<Options, "container" | "log">,
-) => {
-  try {
-    await container.remove((details?.container ?? defaults.container)(browser));
-    if (details?.log) console.log(`Removed existing container for ${browser}`);
-  } catch {}
-};
-
 /**
  *
  * @param BROWSER
@@ -50,7 +40,9 @@ export const buildAndRun = async (BROWSER: Browser, details?: Options) => {
   const name = (details?.container ?? defaults.container)(BROWSER);
   const tag = (details?.image ?? defaults.image)(BROWSER);
 
-  await tryRemove(BROWSER, details);
+  if (details?.log)
+    console.log(`(Try) Removing existing container for ${BROWSER}`);
+  await container.tryRemove(name);
 
   if (details?.log) console.log(`Building image ${tag} from ${context}...`);
 
@@ -213,10 +205,7 @@ export const sessionWithTabs = async (
    */
   const advance = () => {};
 
-  const withTabSelected = <Return>(
-    index: number,
-    fn: () => Return,
-  ): Promise<Awaited<Return>> => {
+  const withTabSelected = <Return>(index: number, fn: () => Return) => {
     const result = queue.then(async () => {
       await selectTab(index);
       return fn();
