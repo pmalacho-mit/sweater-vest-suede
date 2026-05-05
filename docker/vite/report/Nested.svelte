d4066d9c5f365333336b22449bb9f514e6ad1836
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2026-05-05 03:10:33 +0000

    adding files for report test

---
+++ docker/vite/report/Nested.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { Sweater } from "./release";
+
+  class Pocket {}
+</script>
+
+<Sweater config>
+  <Sweater body={async (harness) => {}}>
+    {#snippet vest(pocket: Pocket)}
+      ...Group 1, Test 1 (nested)...
+    {/snippet}
+  </Sweater>
+  <Sweater body={async (harness) => {}}>
+    {#snippet vest(pocket: Pocket)}
+      ...Group 1, Test 2 (nested)...
+    {/snippet}
+  </Sweater>
+</Sweater>
+
+<Sweater config class="custom-config-class" style="color: blue;">
+  <Sweater body={async (harness) => {}}>
+    {#snippet vest(pocket: Pocket)}
+      ...Group 2, Test 1 (nested)...
+    {/snippet}
+  </Sweater>
+  <Sweater body={async (harness) => {}}>
+    {#snippet vest(pocket: Pocket)}
+      ...Group 2, Test 2 (nested)...
+    {/snippet}
+  </Sweater>
+</Sweater>
+
+<style>
+  :global(.custom-config-class) {
+    background-color: greenyellow;
+  }
+</style>
